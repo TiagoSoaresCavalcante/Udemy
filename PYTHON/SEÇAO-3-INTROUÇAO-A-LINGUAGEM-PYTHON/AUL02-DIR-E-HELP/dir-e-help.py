896598b320b557d2efbd9937fded162aa404a620
TiagoSoaresCavalcante <166626393+TiagoSoaresCavalcante@users.noreply.github.com>
2024-04-12 14:32:08 -0300

    Create dir-e-help.py
    AUTERADO

---
+++ PYTHON/SEÇAO-3-INTROUÇAO-A-LINGUAGEM-PYTHON/AUL02-DIR-E-HELP/dir-e-help.py
@@ -0,0 +1,8 @@
+"""
+UTILITARIOS PARA AUXILIAR NA PROGRAMAÇÃO
+Dir -> mostra todos os aattributos e funções/metodos disponivel para 
+determinar tipo de dados ou variavel.
+
+Help -> Aprsenta a documentação/como utillizar os atributos, propiedades e funções
+metodos disponivell para detterminnado tipo de dado ou variaval
+"""
